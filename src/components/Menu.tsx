48b886ec34bcba935a12288f217e552978a4cb4f
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-06-12 15:24:07 +0400

    working on menu!

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,50 +1,61 @@
 import { Link } from "react-router-dom";
-
 export default function Menu() {
   return (
-    <nav className="navigation-menu font-spartan  font-bold leading-[25px] tracking-[1.364px]">
-      <ul>
-        <li>
-          <Link className="text-[#FFF]  hover:text-[#FFF]" to="/mercury">
-            Mercury
-          </Link>
-        </li>
-        <li>
-          <Link className="text-[#FFF]  hover:text-[#FFF]" to="/venus">
-            Venus
-          </Link>
-        </li>
-        <li>
-          <Link className="text-[#FFF]  hover:text-[#FFF]" to="/earth">
-            Earth
-          </Link>
-        </li>
-        <li>
-          <Link className="text-[#FFF]  hover:text-[#FFF]" to="/mars">
-            Mars
-          </Link>
-        </li>
-        <li>
-          <Link className="text-[#FFF]  hover:text-[#FFF]" to="/jupiter">
-            Jupiter
-          </Link>
-        </li>
-        <li>
-          <Link className="text-[#FFF]  hover:text-[#FFF]" to="/saturn">
-            Saturn
-          </Link>
-        </li>
-        <li>
-          <Link className="text-[#FFF]  hover:text-[#FFF]" to="/uranus">
-            Uranus
-          </Link>
-        </li>
-        <li>
-          <Link className="text-[#FFF]  hover:text-[#FFF]" to="/neptune">
-            Neptune
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <header className="flex justify-between align-center">
+        <h1 className="font-normal text-[28px] tracking-[-1.05px] leading-normal">
+          the planets
+        </h1>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="17">
+          <g fill="#FFF" fill-rule="evenodd">
+            <path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z" />
+          </g>
+        </svg>
+      </header>
+      <nav className="navigation-menu font-spartan  font-bold leading-[25px] tracking-[1.364px]">
+        <ul>
+          <li>
+            <Link className="  " to="/mercury">
+              Mercury
+            </Link>
+          </li>
+          <li>
+            <Link className="" to="/venus">
+              Venus
+            </Link>
+          </li>
+          <li>
+            <Link className="" to="/earth">
+              Earth
+            </Link>
+          </li>
+          <li>
+            <Link className="" to="/mars">
+              Mars
+            </Link>
+          </li>
+          <li>
+            <Link className="" to="/jupiter">
+              Jupiter
+            </Link>
+          </li>
+          <li>
+            <Link className="" to="/saturn">
+              Saturn
+            </Link>
+          </li>
+          <li>
+            <Link className="" to="/uranus">
+              Uranus
+            </Link>
+          </li>
+          <li>
+            <Link className="" to="/neptune">
+              Neptune
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
