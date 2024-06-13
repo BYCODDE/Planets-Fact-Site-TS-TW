c4c3fd2b6be5fd4d4db2702a59c9b25bb16ca089
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-06-13 18:02:29 +0400

    working on bottom part of singleplanet!

--- src/pages/Singleplanet.tsx
+++ src/pages/Singleplanet.tsx
@@ -3,17 +3,19 @@ import data from "../data.json";
 import { useParams } from "react-router-dom";
 import { BurgerProps } from "../components/Menu";
 import bgPattern from "../assets/background-stars.svg";
+
 const Singleplanet: React.FC<BurgerProps> = ({
   Burger,
   setBurger,
   burger_svg,
 }) => {
   const { name } = useParams();
-  console.log(name);
   const currentPlanet = data.find(
     (planet) => planet.name.toLowerCase() === name?.toLowerCase()
   );
 
+
+  const planetImagePath = currentPlanet?.images.planet;
   console.log(currentPlanet);
 
   return (
@@ -33,8 +35,22 @@ const Singleplanet: React.FC<BurgerProps> = ({
           <h3 className="cursor-pointer">Surface</h3>
         </div>
         <hr className="h-[1px] w-full opacity-10 mt-[20px] absolute top-[121px] right-0 left-0  " />
+        <div className="flex justify-center mt-[95px] mb-[98px]">
+          <img
+            src={planetImagePath}
+            className="w-[111px] h-[111px] "
+            alt="planet_img"
+          />
+        </div>
+        <div className="text-[#FFFFFF] text-center">
+          <h2 className=" text-[40px]">{currentPlanet?.name}</h2>
+          <p className="font-spartan  text-[11px] leading-[22px]  normal-case font-normal opacity-70 mt-[16px]  mb-[32px]">
+            {currentPlanet?.overview.content}
+          </p>
+        </div>
       </div>
     </>
   );
 };
+
 export default Singleplanet;
