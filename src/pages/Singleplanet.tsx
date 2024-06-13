f020038e3ae97dc95e4e595b826db6a66a71b773
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-06-13 16:46:35 +0400

    working on single planet now top part is done any few visiuals are left!

--- src/pages/Singleplanet.tsx
+++ src/pages/Singleplanet.tsx
@@ -3,7 +3,11 @@ import data from "../data.json";
 import { useParams } from "react-router-dom";
 import { BurgerProps } from "../components/Menu";
 
-const Singleplanet: React.FC<BurgerProps> = ({ Burger, setBurger, burger_svg }) => {
+const Singleplanet: React.FC<BurgerProps> = ({
+  Burger,
+  setBurger,
+  burger_svg,
+}) => {
   const { name } = useParams();
   console.log(name);
   const currentPlanet = data.find(
@@ -15,7 +19,13 @@ const Singleplanet: React.FC<BurgerProps> = ({ Burger, setBurger, burger_svg }) 
   return (
     <>
       <div className="flex flex-col p-[24px]">
-        <Header Burger={Burger} setBurger={setBurger} burger_svg={burger_svg}  />
+        <Header Burger={Burger} setBurger={setBurger} burger_svg={burger_svg} />
+        <div className="flex  font-bold text-[9px]  tracking-[1.929px] font-spartan mt-[40px] justify-between">
+          <h3 className="cursor-pointer">OVERVIEW</h3>
+          <h3 className="cursor-pointer">Structure</h3>
+          <h3 className="cursor-pointer">Surface</h3>
+        </div>
+        <hr className="h-[1px] w-full opacity-10 mt-[20px] absolute top-[121px] right-0 left-0  " />
       </div>
     </>
   );
