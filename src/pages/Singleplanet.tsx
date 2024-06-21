632ba492809611f282b47bc401445ba21f8f4969
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-06-21 18:54:35 +0400

    main stuff is done! few styles are left only!

--- src/pages/Singleplanet.tsx
+++ src/pages/Singleplanet.tsx
@@ -17,40 +17,69 @@ const Singleplanet: React.FC<BurgerProps> = ({
   );
 
   const isMediumDevice = useMediaQuery("only screen and (min-width : 769px)");
-  const isLargeDevice = useMediaQuery(
-    "only screen and (min-width : 993px) "
-  );
-type PlanetName =
-  | "Mercury"
-  | "Venus"
-  | "Earth"
-  | "Mars"
-  | "Jupiter"
-  | "Saturn"
-  | "Uranus"
-  | "Neptune";
+  const isLargeDevice = useMediaQuery("only screen and (min-width : 993px) ");
+  type PlanetName =
+    | "Mercury"
+    | "Venus"
+    | "Earth"
+    | "Mars"
+    | "Jupiter"
+    | "Saturn"
+    | "Uranus"
+    | "Neptune";
 
   const getPlanetClass = (planetName: PlanetName | undefined): string => {
     switch (planetName) {
       case "Venus":
-        return isMediumDevice ? "w-[253px] h-[253px]" : "w-[154px] h-[154px]";
+        return isLargeDevice
+          ? "w-[400px] h-[400px]"
+          : isMediumDevice
+          ? "w-[253px] h-[253px]"
+          : "w-[154px] h-[154px]";
       case "Earth":
-        return isMediumDevice ? "w-[285px] h-[285px]" : "w-[173px] h-[173px]";
+        return isLargeDevice
+          ? "w-[450px] h-[450px]"
+          : isMediumDevice
+          ? "w-[285px] h-[285px]"
+          : "w-[173px] h-[173px]";
       case "Mars":
-        return isMediumDevice ? "w-[213px] h-[213px]" : "w-[129px] h-[129px]";
+        return isLargeDevice
+          ? "w-[336px] h-[336px]"
+          : isMediumDevice
+          ? "w-[213px] h-[213px]"
+          : "w-[129px] h-[129px]";
       case "Jupiter":
-        return isMediumDevice ? "w-[369px] h-[369px]" : "w-[224px] h-[224px]";
+        return isLargeDevice
+          ? "w-[582px] h-[582px]"
+          : isMediumDevice
+          ? "w-[369px] h-[369px]"
+          : "w-[224px] h-[224px]";
       case "Saturn":
-        return isMediumDevice ? "w-[340px] h-[340px]" : "w-[256px] h-[256px]";
+        return isLargeDevice
+          ? "w-[416px] h-[416px]"
+          : isMediumDevice
+          ? "w-[340px] h-[340px]"
+          : "w-[256px] h-[256px]";
       case "Uranus":
-        return isMediumDevice ? "w-[290px] h-[290px]" : "w-[176px] h-[176px]";
+        return isLargeDevice
+          ? "w-[458px] h-[458px]"
+          : isMediumDevice
+          ? "w-[290px] h-[290px]"
+          : "w-[176px] h-[176px]";
       case "Neptune":
-        return isMediumDevice ? "w-[285px] h-[285px]" : "w-[173px] h-[173px]";
+        return isLargeDevice
+          ? "w-[450px] h-[450px]"
+          : isMediumDevice
+          ? "w-[285px] h-[285px]"
+          : "w-[173px] h-[173px]";
       default:
-        return isMediumDevice ? "w-[184px] h-[184px]" : "w-[111px] h-[111px]";
+        return isLargeDevice
+          ? "w-[290px] h-[290px]"
+          : isMediumDevice
+          ? "w-[184px] h-[184px]"
+          : "w-[111px] h-[111px]";
     }
   };
-
 
   const getPlanetStyles = (planetName: PlanetName | undefined): string => {
     const commonStyles = "border-b-4 pb-[11px]";
@@ -87,7 +116,6 @@ type PlanetName =
 
     return isMediumDevice ? "bg-[#419EBB]" : `${commonStyles} border-[#419EBB]`;
   };
-
 
   const planetName: PlanetName | undefined = currentPlanet?.name as
     | PlanetName
@@ -132,7 +160,11 @@ type PlanetName =
             />
           ) : null}
           {isLargeDevice ? (
-            <div className="flex gap-[33px]">
+            <div
+              className={`${
+                isLargeDevice ? "text-[18px]" : ""
+              } flex gap-[33px]`}
+            >
               <Link to="/planet/mercury/overview">Mercury</Link>
               <Link to="/planet/venus/overview">Venus</Link>
               <Link to="/planet/earth/overview">Earth</Link>
@@ -189,53 +221,138 @@ type PlanetName =
       "h-[1px] w-full opacity-10  absolute top-[121px] right-0 left-0"`}
       />
 
-      <div className="flex justify-center mt-[95px] mb-[98px]">
-        {details === "structure" ? (
-          <img
-            src={planetImagePath2}
-            className={planetClass}
-            alt="planet_structure"
-          />
-        ) : details === "surface" ? (
-          <div className="relative">
+      <div
+        className={`${
+          isLargeDevice ? " flex justify-around items-center mt-[130px]" : ""
+        }`}
+      >
+        <div className="flex justify-center mt-[95px] mb-[98px]">
+          {details === "structure" ? (
+            <img
+              src={planetImagePath2}
+              className={planetClass}
+              alt="planet_structure"
+            />
+          ) : details === "surface" ? (
+            <div className="relative">
+              <img
+                src={planetImagePath}
+                className={planetClass}
+                alt="planet_surface"
+              />
+              <img
+                src={planetImagePath3}
+                className="absolute w-[80%] h-[160px]  left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                alt="planet_geology"
+              />
+            </div>
+          ) : (
             <img
               src={planetImagePath}
               className={planetClass}
-              alt="planet_surface"
+              alt="planet_default"
             />
-            <img
-              src={planetImagePath3}
-              className="absolute w-[80%] h-[160px]  left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-              alt="planet_geology"
-            />
+          )}
+        </div>
+        {isMediumDevice ? (
+          <div
+            className={`${
+              isLargeDevice ? "flex-col -" : "justify-between items-center"
+            } text-[#FFFFFF] flex  p-[30px]`}
+          >
+            <div className={`${isMediumDevice ? "max-w-[339px]" : ""}`}>
+              {" "}
+              <h2
+                className={`${
+                  isLargeDevice ? "text-[80px]" : "text-[48px]"
+                }   `}
+              >
+                {currentPlanet?.name}
+              </h2>
+              {details === "structure" ? (
+                <p className="font-spartan text-[15px] leading-[22px] normal-case font-normal opacity-80 mt-[16px] mb-[32px]">
+                  {currentPlanet?.structure.content}
+                </p>
+              ) : details === "surface" ? (
+                <p className="font-spartan text-[15px] leading-[22px] normal-case font-normal opacity-80 mt-[16px] mb-[32px]">
+                  {currentPlanet?.geology.content}
+                </p>
+              ) : (
+                <p className="font-spartan text-[15px] leading-[22px] normal-case font-normal opacity-80 mt-[16px] mb-[32px]">
+                  {currentPlanet?.overview.content}
+                </p>
+              )}
+              <div className={` flex`}>
+                <a
+                  className={`${
+                    isLargeDevice ? "text-[15px]" : ""
+                  } normal-case text-[#FFFFFF] opacity-50 text-[12px] font-spartan font-[300] leading-[25px] tracking-[1.5px] mb-[28px]`}
+                  href={wikipediaLink}
+                >
+                  Source :{" "}
+                  <span className="underline font-bold">Wikipedia</span>
+                  <img
+                    src="/assets/icon-source.svg"
+                    alt="icon-source"
+                    className="inline-block ml-[5px] font-bold"
+                  />
+                </a>
+              </div>
+            </div>
+            <div>
+              <div className="flex font-bold text-[15px] tracking-[2px] font-spartan mt-[40px] justify-center flex-col  leading-[25px] gap-[16px]">
+                <Link
+                  className={`border border-[#fff] border-opacity-20 max-w-[340px] max-h-[40px] p-[24px] flex justify-left items-center gap-[17px] font-[400] ${
+                    isMediumDevice && details === "overview" && planetStyles
+                  }`}
+                  to={`/planet/${currentPlanet?.name}/overview`}
+                >
+                  <span className="opacity-50">01</span>
+                  <p>OVERVIEW</p>
+                </Link>
+
+                <Link
+                  className={`border border-[#fff] border-opacity-20 max-w-[340px] max-h-[40px] p-[24px] flex justify-left items-center gap-[17px] font-[400] ${
+                    isMediumDevice && details === "structure" && planetStyles
+                  }`}
+                  to={`/planet/${currentPlanet?.name}/structure`}
+                >
+                  <span className="opacity-50">02</span>
+                  <p>Internal Structure</p>
+                </Link>
+
+                <Link
+                  className={`border border-[#fff] border-opacity-20 max-w-[340px] max-h-[40px] p-[24px] flex justify-left items-center gap-[17px] font-[400] ${
+                    isMediumDevice && details === "surface" && planetStyles
+                  }`}
+                  to={`/planet/${currentPlanet?.name}/surface`}
+                >
+                  <span className="opacity-50">03</span>
+                  <p>Surface Geology</p>
+                </Link>
+              </div>
+            </div>
           </div>
         ) : (
-          <img
-            src={planetImagePath}
-            className={planetClass}
-            alt="planet_default"
-          />
-        )}
-      </div>
-      {isMediumDevice ? (
-        <div className="text-[#FFFFFF] flex justify-between items-center p-[30px]">
-          <div className={`${isMediumDevice ? "max-w-[339px]" : ""}`}>
-            {" "}
-            <h2 className="text-[48px]">{currentPlanet?.name}</h2>
-            {details === "structure" ? (
-              <p className="font-spartan text-[15px] leading-[22px] normal-case font-normal opacity-80 mt-[16px] mb-[32px]">
-                {currentPlanet?.structure.content}
-              </p>
-            ) : details === "surface" ? (
-              <p className="font-spartan text-[15px] leading-[22px] normal-case font-normal opacity-80 mt-[16px] mb-[32px]">
-                {currentPlanet?.geology.content}
-              </p>
-            ) : (
-              <p className="font-spartan text-[15px] leading-[22px] normal-case font-normal opacity-80 mt-[16px] mb-[32px]">
-                {currentPlanet?.overview.content}
-              </p>
-            )}
-            <div className="flex  ">
+          <>
+            <div className="text-[#FFFFFF] text-center">
+              <h2 className="text-[40px]">{currentPlanet?.name}</h2>
+              {details === "structure" ? (
+                <p className="font-spartan text-[11px] leading-[22px] normal-case font-normal opacity-80 mt-[16px] mb-[32px]">
+                  {currentPlanet?.structure.content}
+                </p>
+              ) : details === "surface" ? (
+                <p className="font-spartan text-[11px] leading-[22px] normal-case font-normal opacity-80 mt-[16px] mb-[32px]">
+                  {currentPlanet?.geology.content}
+                </p>
+              ) : (
+                <p className="font-spartan text-[11px] leading-[22px] normal-case font-normal opacity-80 mt-[16px] mb-[32px]">
+                  {currentPlanet?.overview.content}
+                </p>
+              )}
+            </div>
+
+            <div className="flex justify-center items-center">
               <a
                 className="normal-case text-[#FFFFFF] opacity-50 text-[12px] font-spartan font-[300] leading-[25px] tracking-[1.5px] mb-[28px]"
                 href={wikipediaLink}
@@ -248,85 +365,20 @@ type PlanetName =
                 />
               </a>
             </div>
-          </div>
-          <div>
-            <div className="flex font-bold text-[15px] tracking-[2px] font-spartan mt-[40px] justify-center flex-col  leading-[25px] gap-[16px]">
-              <Link
-                className={`border border-[#fff] border-opacity-20 max-w-[340px] max-h-[40px] p-[24px] flex justify-left items-center gap-[17px] font-[400] ${
-                  isMediumDevice && details === "overview" && planetStyles
-                }`}
-                to={`/planet/${currentPlanet?.name}/overview`}
-              >
-                <span className="opacity-50">01</span>
-                <p>OVERVIEW</p>
-              </Link>
-
-              <Link
-                className={`border border-[#fff] border-opacity-20 max-w-[340px] max-h-[40px] p-[24px] flex justify-left items-center gap-[17px] font-[400] ${
-                  isMediumDevice && details === "structure" && planetStyles
-                }`}
-                to={`/planet/${currentPlanet?.name}/structure`}
-              >
-                <span className="opacity-50">02</span>
-                <p>Internal Structure</p>
-              </Link>
-
-              <Link
-                className={`border border-[#fff] border-opacity-20 max-w-[340px] max-h-[40px] p-[24px] flex justify-left items-center gap-[17px] font-[400] ${
-                  isMediumDevice && details === "surface" && planetStyles
-                }`}
-                to={`/planet/${currentPlanet?.name}/surface`}
-              >
-                <span className="opacity-50">03</span>
-                <p>Surface Geology</p>
-              </Link>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <>
-          <div className="text-[#FFFFFF] text-center">
-            <h2 className="text-[40px]">{currentPlanet?.name}</h2>
-            {details === "structure" ? (
-              <p className="font-spartan text-[11px] leading-[22px] normal-case font-normal opacity-80 mt-[16px] mb-[32px]">
-                {currentPlanet?.structure.content}
-              </p>
-            ) : details === "surface" ? (
-              <p className="font-spartan text-[11px] leading-[22px] normal-case font-normal opacity-80 mt-[16px] mb-[32px]">
-                {currentPlanet?.geology.content}
-              </p>
-            ) : (
-              <p className="font-spartan text-[11px] leading-[22px] normal-case font-normal opacity-80 mt-[16px] mb-[32px]">
-                {currentPlanet?.overview.content}
-              </p>
-            )}
-          </div>
-
-          <div className="flex justify-center items-center">
-            <a
-              className="normal-case text-[#FFFFFF] opacity-50 text-[12px] font-spartan font-[300] leading-[25px] tracking-[1.5px] mb-[28px]"
-              href={wikipediaLink}
-            >
-              Source : <span className="underline font-bold">Wikipedia</span>
-              <img
-                src="/assets/icon-source.svg"
-                alt="icon-source"
-                className="inline-block ml-[5px] font-bold"
-              />
-            </a>
-          </div>
-        </>
-      )}
+          </>
+        )}
+      </div>
 
       <div
         className={`${
           isMediumDevice
             ? "flex-row gap-0 justify-between p-[30px] pt-[0px]"
             : "flex-col"
+        } ${
+          isLargeDevice ? "mt-[90px]" : ""
         } flex justify-center  gap-[10px] pb-[30px] text-[#FFFFFF] font-[500]`}
       >
-
-{/* TODO:////////////////////////////////////////////////////////// */}
+        {/* TODO:////////////////////////////////////////////////////////// */}
 
         <div
           className={`${
